034a71e5bf89e56504fd11c6f3101e6ceaecf65a
inductive-anks1 <ankit.kumar@plaksha.edu.in>
2024-08-24 05:04:08 +0000

    Final Version

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from tensorflow.keras.models import load_model
 import keras_nlp
 from keras_nlp.models import DistilBertClassifier, DistilBertBackbone, DistilBertPreprocessor, DistilBertTokenizer
 
-MODEL_SAVE_PATH = '/home/inductive-anks/Predict-AI-Generated-Text/Detect-AI-Generated-Text/models/trained_model.keras'
+MODEL_SAVE_PATH = '/home/inductive-anks/kaggle/Detect-AI-Generated-Text/models/my_model_test_kaggle.keras'
 
-# Ensure that all the necessary classes are included in custom_objects
 custom_objects = {
     'DistilBertClassifier': DistilBertClassifier,
     'DistilBertBackbone': DistilBertBackbone,
@@ -16,37 +15,33 @@ custom_objects = {
 
 classifier = load_model(MODEL_SAVE_PATH, custom_objects=custom_objects)
 
-# Streamlit app title
 st.title("AI-Generated Text Detection")
 
-# Text input from user
 user_input = st.text_area("Enter the text you want to analyze:")
 
-# If the user provides input, make a prediction
 if st.button("Analyze Text"):
     if user_input.strip() == "":
         st.warning("Please enter some text for analysis.")
     else:
-        # Convert input text to a pandas Series
+    
         input_data = pd.Series([user_input])
 
-        # Make predictions directly without preprocessing
         prediction_probs = classifier.predict(input_data)
 
-        # Extract the probability of AI-generated text
         ai_generated_prob = prediction_probs[0][1]
 
-        # Display the result with likelihood interpretation
         st.write(f"**Probability of the text being AI-generated:** {ai_generated_prob:.2%}")
 
-        if ai_generated_prob > 0.75:
-            st.warning("The model suggests that this text is **highly likely** AI-generated.")
-        elif ai_generated_prob > 0.5:
-            st.warning("The model suggests that this text is **likely** AI-generated.")
-        elif ai_generated_prob > 0.25:
-            st.info("The model suggests that this text is **somewhat likely** AI-generated.")
-        else:
-            st.success("The model suggests that this text is **unlikely** to be AI-generated.")
+
+        st.warning(f"The model suggests that this text is {ai_generated_prob:.2%} likely to be AI-generated.")
+        # if ai_generated_prob > 0.75:
+        #     st.warning("The model suggests that this text is **highly likely** AI-generated.")
+        # elif ai_generated_prob > 0.5:
+        #     st.warning("The model suggests that this text is **likely** AI-generated.")
+        # elif ai_generated_prob > 0.25:
+        #     st.info("The model suggests that this text is **somewhat likely** AI-generated.")
+        # else:
+        #     st.success("The model suggests that this text is **unlikely** to be AI-generated.")
 
 # Footer
 st.write("---")
